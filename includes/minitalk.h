51eed23ea9e2039fcd88575ace7bd802c52556c8
Emran Binjamaan <therealemran.design@gmail.com>
2024-01-14 09:55:50 +0400

    Set up minitalk's header

---
+++ includes/minitalk.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebinjama <ebinjama@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/14 09:28:26 by ebinjama          #+#    #+#             */
+/*   Updated: 2024/01/14 09:47:12 by ebinjama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <stdint.h>
+# include <signal.h>
+# include <stdbool.h>
+# include "../libft/includes/libft.h"
+# include "stdlog.h"
+
+#endif // !MINITALK_H
