2643b5eac6c4538ec1f4b55cc949f1041f35bd25
Emran Binjamaan <therealemran.design@gmail.com>
2024-01-14 09:55:32 +0400

    Custom logging messages

---
+++ includes/stdlog.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stdlog.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebinjama <ebinjama@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/14 09:32:23 by ebinjama          #+#    #+#             */
+/*   Updated: 2024/01/14 09:46:14 by ebinjama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STDLOG_H
+# define STDLOG_H
+
+# define RED "\x1b[31m"
+# define DEF "\x1b[0m"
+
+# define BAD_SIGNAL "Error: BAD SIGNAL"
+# define BAD_INPUT "Input must be valid, habibi."
+# define EASTER_EGG "You tryna crash your device habibi?"
+# define BAD_FORMAT "Format:\n\t./client [PID] [message]"
+
+#endif // !STDLOG_H
